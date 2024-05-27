b887af456d6415af38367bb7da1f0ce933daedf8
Eric Schoebel <wir13fjs@studserv.uni-leipzig.de>
2024-05-27 18:05:28 +0200

    deleted dead code

--- frontend.py
+++ frontend.py
@@ -1,7 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
-import os
-import pandas as pd
 from handle_documents import load_documents, search_documents
 
 class Ui_MainWindow(object):
